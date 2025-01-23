052fca06ae3a41bcdbd7caae78abf9ad0ab21d21
unknown <sebastian.cornejo.olea@gmail.com>
2025-01-23 12:29:31 -0300

    se suben fuentes para su respaldo

---
+++ Juegos/My project (2)/Assets/scripts/CamaraControl.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class CamaraControl : MonoBehaviour
+{
+  public Transform objetivo;
+  public float velocidadCamara = 0.025f;
+  public Vector3 desplazamiento;
+
+  private void LateUpdate()
+  {
+    Vector3 posicionDeseada = objetivo.position + desplazamiento;
+
+    Vector3 posicionSuavizada = Vector3.Lerp(transform.position, posicionDeseada, velocidadCamara);
+
+    transform.position = posicionSuavizada;
+  }
+}
---
+++ Juegos/My project (2)/Assets/scripts/PausarJuego.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class PausarJuego : MonoBehaviour
+{
+    public GameObject menuPausa;
+    public bool juegoPausado = false;
+
+    private void Start()
+    {
+        
+    }
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(juegoPausado)
+            {
+                Reanudar();
+            }
+            else
+            {
+                Pausar();
+            }
+        }
+    }
+
+    public void Reanudar()
+    {
+        menuPausa.SetActive(false);
+        Time.timeScale =1;
+        juegoPausado = false;
+    }
+
+    public void Pausar()
+    {
+        menuPausa.SetActive(true);
+        Time.timeScale =0;
+        juegoPausado = true;
+    }
+}
